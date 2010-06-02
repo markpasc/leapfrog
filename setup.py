fb3ddd02a0b880927c9f3521e1cc6e47c371dba7
Mark Paschal <markpasc@markpasc.org>
2010-06-02 15:38:57 -0700

    Be installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='giraffe',
+    version='1.0',
+    packages=['giraffe'],
+    include_package_data=True,
+
+    requires=['Django'],
+    install_requires=['Django'],
+)
