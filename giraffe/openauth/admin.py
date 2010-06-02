e06a8fc3d47c2ccfe11d62715d759d32f9fae77e
Mark Paschal <markpasc@markpasc.org>
2010-06-02 16:26:54 -0700

    Link to unimplemented 'import friends' admin action

--- giraffe/openauth/admin.py
+++ giraffe/openauth/admin.py
@@ -1,3 +1,4 @@
+from django.conf.urls.defaults import url, patterns
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
@@ -14,6 +15,20 @@ class OpenIDInline(admin.TabularInline):
 class GiraffeUserAdmin(UserAdmin):
 
     inlines = [OpenIDInline]
+
+    def import_friends(self, request):
+        # Find this user's openids.
+        # Find those openids' friends.
+        # Find which of those friend openids don't already exist.
+        # Provide a form for associating those with users and groups.
+        raise NotImplementedError
+
+    def get_urls(self):
+        urls = super(GiraffeUserAdmin, self).get_urls()
+        extra_urls = patterns('',
+            url(r'^import_friends/$', self.admin_site.admin_view(self.import_friends), name='admin_import_friends'),
+        )
+        return extra_urls + urls  # me first
 
 
 class UserOpenIDAdmin(admin.ModelAdmin):
