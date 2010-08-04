861547fd4f526395f07ac85d35a9f92f0844a555
Mark Paschal <markpasc@markpasc.org>
2010-08-04 13:47:35 -0700

    Add atom_id and published date fields

---
+++ giraffe/publisher/migrations/0005_auto__add_field_asset_published.py
@@ -0,0 +1,80 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Asset.published'
+        db.add_column('publisher_asset', 'published', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Asset.published'
+        db.delete_column('publisher_asset', 'published')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'publisher.asset': {
+            'Meta': {'object_name': 'Asset'},
+            'atom_id': ('django.db.models.fields.CharField', [], {'default': "u'tag:example.com,2010:3270374045903229104'", 'unique': 'True', 'max_length': '200', 'blank': 'True'}),
+            'content': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'published': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
+            'summary': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'})
+        },
+        'publisher.subscription': {
+            'Meta': {'unique_together': "(('callback', 'topic'),)", 'object_name': 'Subscription'},
+            'callback': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lease_until': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'secret': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'topic': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['publisher']
--- giraffe/publisher/models.py
+++ giraffe/publisher/models.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+from random import randint
+import sys
+
+from django.contrib.sites.models import Site
 from django.contrib.auth.models import User
 from django.db import models
 from django.template.defaultfilters import truncatewords
@@ -8,7 +13,9 @@ class Asset(models.Model):
     title = models.CharField(max_length=200, blank=True)
     summary = models.TextField(blank=True)
     content = models.TextField(blank=True)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
+    atom_id = models.CharField(max_length=200, unique=True, blank=True)
+    published = models.DateTimeField(default=datetime.now)
 
     @property
     def preview(self):
@@ -16,11 +23,18 @@ class Asset(models.Model):
         return truncatewords(text, 10)
 
     def __unicode__(self):
-        return self.title
+        return self.title or truncatewords(self.summary, 10) or u'No title'
 
     @models.permalink
     def get_absolute_url(self):
         return ('publisher-asset', (), {'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.atom_id:
+            self.atom_id = 'tag:%s,2010:%s' % (Site.objects.get_current().domain, self.slug)
+        if self.published is None:
+            self.published = datetime.now()
+        return super(Asset, self).save(*args, **kwargs)
 
 
 class Subscription(models.Model):
