d66d515deb7f9991b7fc7f3b15c73646c38fea9a
Mark Paschal <markpasc@markpasc.org>
2010-08-11 13:05:36 -0700

    Try to kind of mostly ask for a subscription

--- giraffe/aggregator/tasks.py
+++ giraffe/aggregator/tasks.py
@@ -1,3 +1,7 @@
+import logging
+from urllib import urlencode
+from xml.etree import ElementTree
+
 import httplib2
 
 from celery.decorators import task
@@ -5,9 +9,40 @@ from celery.decorators import task
 
 @task
 def subscribe(feed_url, sub_pk):
-    http = httplib2.Http()
-    resp, cont = h.request(feed_url)
+    log = logging.getLogger('%s.subscribe' % __name__)
 
-    # Look for hub link.
+    h = httplib2.Http()
+    resp, cont = h.request(feed_url)
+    if resp.status != 200:
+        # No feed means we can't subscribe to notifications.
+        log.warning('HTTP response %d %s trying to fetch feed %s', resp.status, resp.reason, feed_url)
+        return
+
+    # Look for the hub link.
+    try:
+        doc = ElementTree.fromstring(cont)
+        hubs = [elem for elem in doc.findall('feed/link') if elem.attrib['rel'] == 'hub']
+        hub_url = hubs[0].attrib['href']
+    except Exception, exc:
+        log.warning('%s trying to find hub in feed %s: %s', type(exc).__name__, feed_url, str(exc))
+        return
+
     # Try to subscribe.
-    # Mark the Subscription as a push sub.
+    callback_root = 'http://%s/' % Site.objects.get_current().domain
+    callback = urljoin(callback_root, reverse('aggregator-callback'))
+    subreq = {
+        'hub.callback': callback,
+        'hub.mode': 'subscribe',
+        'hub.topic': feed_url,
+        'hub.verify': 'async',
+    }
+    h.request(uri=hub_url, method='POST', body=urlencode(subreq),
+        headers={'Content-Type': 'application/x-www-form-urlencoded'})
+
+    if resp.status not in (202, 204):
+        log.warning('HTTP response %d %s trying to request subscription to feed %s from hub %s',
+            resp.status, resp.reason, feed_url, hub_url)
+
+    # Even if the response was correct, we want to mark the subscription as
+    # push when the hub verifies the subscription, so we're done.
+    return
