b86a684b4f61b7218698e788cceda7d95f1d7b33
Mark Paschal <markpasc@markpasc.org>
2010-08-27 12:16:55 -0700

    Note the new BeautifulSoup requirement too

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     packages=['giraffe'],
     include_package_data=True,
 
-    requires=['Django', 'south', 'celery', 'django-celery', 'httplib2', 'passogva'],
-    install_requires=['Django', 'south', 'celery', 'django-celery', 'httplib2', 'passogva'],
+    requires=['Django', 'south', 'celery', 'django-celery', 'httplib2', 'passogva', 'BeautifulSoup(<3.1)'],
+    install_requires=['Django', 'south', 'celery', 'django-celery', 'httplib2', 'passogva', 'BeautifulSoup<3.1'],
 )
