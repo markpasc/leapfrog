620c56ef2a7e97d2fccb5fc38ad87927b493d0c7
Mark Paschal <markpasc@markpasc.org>
2010-08-27 17:02:59 -0700

    Try to nice up friends app admins

--- giraffe/friends/admin.py
+++ giraffe/friends/admin.py
@@ -1,7 +1,20 @@
+from django import forms
 from django.contrib import admin
 from giraffe.friends import models
 
 
-admin.site.register(models.Person)
-admin.site.register(models.Group)
+class PersonAdmin(admin.ModelAdmin):
+
+    list_display = ('display_name', 'user')
+    search_fields = ('display_name',)
+    filter_horizontal = ('groups',)
+
+
+class GroupAdmin(admin.ModelAdmin):
+
+    list_display = ('display_name', 'tag')
+
+
+admin.site.register(models.Person, PersonAdmin)
+admin.site.register(models.Group, GroupAdmin)
 admin.site.register(models.Identity)
