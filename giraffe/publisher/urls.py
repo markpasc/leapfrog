3bba893db9541e2296e5a2b02c8eae6594c2b4b3
Mark Paschal <markpasc@markpasc.org>
2010-08-30 11:59:00 -0700

    Pages (and footer and add'l styles)

--- giraffe/publisher/urls.py
+++ giraffe/publisher/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('giraffe.publisher.views',
     url(r'^$', 'index', name='publisher-index'),
+    url(r'^page/(?P<page>\d+)', 'index', name='publisher-index-page'),
     url(r'^feed$', 'index', {'template': 'publisher/feed.xml', 'content_type': 'text/plain'}, name='publisher-feed'),
     url(r'^asset/(?P<slug>[\w-]+)$', 'asset', name='publisher-asset'),
 
