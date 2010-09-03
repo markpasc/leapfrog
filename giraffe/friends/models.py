650ca1ddf55d0041cbc52e0cb1e6c94fcd86a67a
Mark Paschal <markpasc@markpasc.org>
2010-09-03 13:56:13 -0700

    Oops, MySQL needs unique fields at most 255 characters

--- giraffe/friends/models.py
+++ giraffe/friends/models.py
@@ -40,7 +40,7 @@ class Group(models.Model):
 class Identity(models.Model):
 
     person = models.ForeignKey(Person)
-    openid = models.CharField(max_length=300, unique=True)
+    openid = models.CharField(max_length=255, unique=True)
 
     def __unicode__(self):
         return self.openid
