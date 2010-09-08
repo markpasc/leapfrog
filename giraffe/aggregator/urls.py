f9998c71f9ba0adf2be86619e93ed03b6ea67256
Martin Atkins <mart@degeneration.co.uk>
2010-09-08 20:27:43 +0000

    Add some basic styling to the aggregator home page.

--- giraffe/aggregator/urls.py
+++ giraffe/aggregator/urls.py
@@ -1,7 +1,16 @@
+
+from os.path import join, dirname
+
 from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('giraffe.aggregator.views',
     url(r'^callback/(?P<sub_pk>\d+)$', 'callback', name='aggregator-callback'),
     url(r'^$', 'activity_stream', name='aggregator-read'),
+)
+
+urlpatterns += patterns('',
+    url(r'^_ga/static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': join(dirname(__file__), 'static')},
+        name='giraffe-static'),
 )
