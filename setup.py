c608e4998b6dabd2bd3a38162c84208d873e91bb
Mark Paschal <markpasc@markpasc.org>
2010-09-20 21:27:18 -0700

    Stupidly switch to using Jinja2 templates for the aggregator app

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     packages=['giraffe'],
     include_package_data=True,
 
-    requires=['Django', 'south', 'celery', 'django_celery', 'httplib2', 'passogva', 'BeautifulSoup(<3.1)'],
-    install_requires=['Django', 'south', 'celery', 'django_celery', 'httplib2', 'passogva', 'BeautifulSoup<3.1'],
+    requires=['Django', 'south', 'celery', 'django_celery', 'httplib2', 'passogva', 'BeautifulSoup(<3.1)', 'jinja2'],
+    install_requires=['Django', 'south', 'celery', 'django_celery', 'httplib2', 'passogva', 'BeautifulSoup<3.1', 'jinja2'],
 )
