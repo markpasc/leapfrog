9fd1c9e1872bc9a3f8c3a512badb3b03626b8ddf
Mark Paschal <markpasc@markpasc.org>
2010-10-06 10:23:13 -0700

    require logging in

--- rhino/urls.py
+++ rhino/urls.py
@@ -8,6 +8,11 @@ urlpatterns = patterns('rhino.views',
 )
 
 urlpatterns += patterns('',
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login',
+        {'template_name': 'rhino/login.jj'}, name='login'),
+)
+
+urlpatterns += patterns('',
     url(r'^static/rhino/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': join(dirname(__file__), 'static')},
         name='rhino-static'),
