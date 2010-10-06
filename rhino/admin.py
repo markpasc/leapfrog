4b716fbaf1ef3e64f8d2b5b7180dea4d0ca73037
Mark Paschal <markpasc@markpasc.org>
2010-10-06 11:00:14 -0700

    update schema

--- rhino/admin.py
+++ rhino/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from rhino import models
 
 
-admin.site.register(models.Subscription)
+admin.site.register(models.Account)
 admin.site.register(models.Object)
 admin.site.register(models.UserStream)
+admin.site.register(models.UserReplyStream)
--- rhino/models.py
+++ rhino/models.py
@@ -1,12 +1,15 @@
+from datetime import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 
 
-class Subscription(models.Model):
+class Account(models.Model):
 
     service = models.CharField(max_length=20)
     name = models.CharField(max_length=100)
     who = models.ForeignKey(User)
+    last_updated = models.DateTimeField(default=datetime.now)
 
 
 class Object(models.Model):
@@ -33,6 +36,17 @@ class Object(models.Model):
 
 class UserStream(models.Model):
 
-    obj = models.ForeignKey(Object)
-    who = models.ForeignKey(User)
+    obj = models.ForeignKey(Object, related_name='stream_items')
+    who = models.ForeignKey(User, related_name='stream_items')
     when = models.DateTimeField(auto_now_add=True)
+
+
+class UserReplyStream(models.Model):
+
+    who = models.ForeignKey(User, related_name='reply_stream_items')
+    root = models.ForeignKey(Object, related_name='reply_reply_stream_items')
+    root_when = models.DateTimeField()
+    reply = models.ForeignKey(Object, related_name='reply_stream_items')
+    reply_when = models.DateTimeField()
+
+    # index: (who, root_when, reply_when)
