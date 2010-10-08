6acdd76360bb6ae18c38388a6f305baca95b5e1e
Mark Paschal <markpasc@markpasc.org>
2010-10-08 16:51:40 -0700

    Import these for convenience

--- rhino/poll/embedlam.py
+++ rhino/poll/embedlam.py
@@ -4,12 +4,13 @@ import urllib2
 
 from BeautifulSoup import BeautifulSoup
 import oembed
+from oembed import OEmbedConsumer, OEmbedEndpoint, OEmbedError
 
 
 log = logging.getLogger(__name__)
 
 
-class DiscoveryConsumer(oembed.OEmbedConsumer):
+class DiscoveryConsumer(OEmbedConsumer):
 
     def _endpointFor(self, url):
         endpoint = super(DiscoveryConsumer, self)._endpointFor(url)
@@ -29,25 +30,25 @@ class DiscoveryConsumer(oembed.OEmbedConsumer):
         try:
             content_type = headers['Content-Type']
         except KeyError:
-            raise oembed.OEmbedError('Resource targeted for discovery has no Content Type')
+            raise OEmbedError('Resource targeted for discovery has no Content Type')
         if not 'html' in content_type.lower():
-            raise oembed.OEmbedError('Resource targeted for discovery is %s, not an HTML page' % content_type)
+            raise OEmbedError('Resource targeted for discovery is %s, not an HTML page' % content_type)
 
         try:
             page = BeautifulSoup(response.read())
         except HTMLParseError:
-            raise oembed.OEmbedError('Could not discover against invalid HTML target %s' % url)
+            raise OEmbedError('Could not discover against invalid HTML target %s' % url)
         head = page.head
         if head is None:
-            raise oembed.OEmbedError('Could not discover against HTML target %s with no head' % url)
+            raise OEmbedError('Could not discover against HTML target %s with no head' % url)
 
         oembed_node = head.find(rel='alternate', type='application/json+oembed')
         if oembed_node is None:
             oembed_node = head.find(rel='alternate', type='text/xml+oembed')
         if oembed_node is None:
-            raise oembed.OEmbedError('Could not discover against HTML target %s with no oembed tags' % url)
+            raise OEmbedError('Could not discover against HTML target %s with no oembed tags' % url)
 
-        return oembed.OEmbedEndpoint(oembed_node['href'])
+        return OEmbedEndpoint(oembed_node['href'])
 
 
 class EmbedError(Exception):
@@ -56,13 +57,13 @@ class EmbedError(Exception):
 
 def embed(url):
     csr = DiscoveryConsumer()
-    endp = oembed.OEmbedEndpoint('http://www.flickr.com/services/oembed',
+    endp = OEmbedEndpoint('http://www.flickr.com/services/oembed',
         ['http://*.flickr.com/*', 'http://flic.kr/*'])
     csr.addEndpoint(endp)
 
     try:
         resource = csr.embed(url)
-    except (oembed.OEmbedError, urllib2.HTTPError), exc:
+    except (OEmbedError, urllib2.HTTPError), exc:
         raise EmbedError('%s trying to embed %s: %s' % (type(exc).__name__, url, str(exc)))
 
     # well
