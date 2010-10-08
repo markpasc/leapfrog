f63d0d7deb54bcd297d9d989ffd13d455e62cf77
Martin Atkins <mart@degeneration.co.uk>
2010-10-07 18:21:23 -0700

    Add a management command to run the typepad and twitter pollers for all user accounts with people attached.

---
+++ rhino/management/commands/fetchnewcontent.py
@@ -0,0 +1,29 @@
+from django.core.management.base import NoArgsCommand, CommandError
+from rhino.models import *
+
+from rhino.tasks import twitter
+from rhino.tasks import typepad
+
+pollers = {
+    "twitter.com": twitter.poll_twitter,
+    "typepad.com": typepad.poll_typepad,
+}
+
+class Command(NoArgsCommand):
+
+    def handle_noargs(self, **options):
+        users = User.objects.all()
+        for user in users:
+            try:
+                for account in user.person.accounts.all():
+                    if account.service in pollers:
+                        poller = pollers[account.service]
+                        poller(account)
+            except Person.DoesNotExist:
+                pass
+
+
+
+
+
+
