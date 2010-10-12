bb8e86021674fb96da616317b4ca02a9901375c2
Mark Paschal <markpasc@markpasc.org>
2010-10-11 20:16:06 -0700

    Show replies

--- rhino/views.py
+++ rhino/views.py
@@ -15,6 +15,13 @@ def home(request):
     stream_items = list(stream_items[:50])
 
     replies = user.reply_stream_items.filter(root_time__range=(stream_items[-1].time, stream_items[0].time)).select_related()
+    reply_by_item = dict()
+    for reply in replies:
+        item_replies = reply_by_item.setdefault(reply.root_id, set())
+        item_replies.add(reply)
+    for item in stream_items:
+        reply_set = reply_by_item.get(item.obj_id, set())
+        item.replies = sorted(iter(reply_set), key=lambda i: i.reply_time)
 
     data = {
         'stream_items': stream_items,
