6a85dc1fb701d0683bff40f226e34b2181be02a7
Mark Paschal <markpasc@markpasc.org>
2010-10-29 14:24:21 -0700

    Support a Zendesk pop-out

--- rhino/context_processors.py
+++ rhino/context_processors.py
@@ -16,4 +16,5 @@ def typekit_code(request):
     return {
         'typekit_code': getattr(settings, 'TYPEKIT_CODE', None),
         'ganalytics_code': getattr(settings, 'GANALYTICS_CODE', None),
+        'zendesk': getattr(settings, 'ZENDESK', None),
     }
