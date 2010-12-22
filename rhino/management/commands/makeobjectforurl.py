eddf4af6d5369d75870d38980da547be9c52955b
Mark Paschal <markpasc@markpasc.org>
2010-12-22 13:29:25 -0500

    spacing

--- rhino/management/commands/makeobjectforurl.py
+++ rhino/management/commands/makeobjectforurl.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
-from rhino.models import *
 
+from rhino.models import *
 from rhino.poll.embedlam import object_for_url
+
 
 class Command(BaseCommand):
 
