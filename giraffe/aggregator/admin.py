4cb740366e2092ffca43b1ac08e9677e04a18c8c
Mark Paschal <markpasc@markpasc.org>
2010-12-27 00:10:53 -0500

    No more giraffe here

--- giraffe/aggregator/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from giraffe.aggregator import models
-
-
-admin.site.register(models.Subscription)
-admin.site.register(models.Activity)
-admin.site.register(models.Object)
