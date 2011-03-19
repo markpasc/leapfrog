03869f02f01e702d932acc87de21617b95bbe828
Mark Paschal <markpasc@markpasc.org>
2011-03-19 00:32:12 -0700

    Don't build dropdowns out of these giantastic tables

--- leapfrog/admin.py
+++ leapfrog/admin.py
@@ -7,6 +7,7 @@ class AccountAdmin(admin.ModelAdmin):
     list_display = ('display_name', 'service', 'ident')
     list_filter = ('service',)
     search_fields = ('display_name', 'ident')
+    raw_id_fields = ('person',)
 
 admin.site.register(models.Account, AccountAdmin)
 
@@ -15,6 +16,7 @@ class ObjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'render_mode', 'external_id', 'time')
     list_filter = ('author', 'render_mode', 'service')
     search_fields = ('title', 'author__display_name', 'foreign_id')
+    raw_id_fields = ('author', 'image', 'in_reply_to')
 
     def external_id(self, obj):
         if obj.service and obj.foreign_id:
@@ -29,12 +31,14 @@ admin.site.register(models.Object, ObjectAdmin)
 class UserStreamAdmin(admin.ModelAdmin):
     list_display = ('obj', 'user', 'time')
     readonly_fields = ('time',)
+    raw_id_fields = ('user', 'obj', 'why_account')
 
 admin.site.register(models.UserStream, UserStreamAdmin)
 
 
 class UserReplyStreamAdmin(admin.ModelAdmin):
     list_display = ('reply', 'root', 'user', 'reply_time')
+    raw_id_fields = ('user', 'root', 'reply')
 
 admin.site.register(models.UserReplyStream, UserReplyStreamAdmin)
 
@@ -43,6 +47,7 @@ class PersonAdmin(admin.ModelAdmin):
     list_display = ('display_name', 'permalink_url', 'user')
     list_filter = ('user',)
     search_fields = ('display_name',)
+    raw_id_fields = ('user',)
 
 admin.site.register(models.Person, PersonAdmin)
 
